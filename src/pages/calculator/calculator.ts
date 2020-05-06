4559012b1d086865590fbc69d6a868e56c3f091d
Carlos Aponte <apontec83@gmail.com>
2020-05-06 00:02:34 -0400

    Added Receive calculation, but still need to be fixed that method.

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -17,14 +17,14 @@ export class CalculatorPage {
 	public porcentaje:number = 5.40;
 	public adicional:number = 0.30;
 	private calcParaRecibir:any = {
-		recibir: "",
-		enviar: "",
-		comision:""
+		recibir: 0,
+		enviar: 0,
+		comision:0
 	}
 	private calcParaEnviar:any = {
-		recibir: "",
-		enviar: "",
-		comision:""
+		recibir: 0,
+		enviar: 0,
+		comision:0
 	}
   constructor(public navCtrl: NavController, public navParams: NavParams) {
 
@@ -40,7 +40,7 @@ export class CalculatorPage {
   	}
   	this.calcParaEnviar.enviar = Number(this.calcParaEnviar.enviar);
 	this.calcParaEnviar.comision = (((this.porcentaje * this.calcParaEnviar.enviar)/100) + this.adicional).toFixed(2);
-	this.calcParaEnviar.recibir = (this.calcParaEnviar.enviar - this.calcParaEnviar.comision).toFixed(2);
+	this.calcParaEnviar.recibir = (parseFloat(this.calcParaEnviar.enviar) - parseFloat(this.calcParaEnviar.comision)).toFixed(2);
 	//console.log("Comision:"+this.calcParaEnviar.comision);
   }
 
@@ -48,7 +48,7 @@ export class CalculatorPage {
 	console.log("calcularRecibo()");
 	this.calcParaRecibir.recibir = Number(this.calcParaRecibir.recibir);
 	this.calcParaRecibir.comision = (((this.porcentaje * this.calcParaRecibir.recibir)/100) + this.adicional).toFixed(2);
-	this.calcParaRecibir.enviar = (this.calcParaRecibir.recibir + this.calcParaRecibir.comision).toFixed(2);
+	this.calcParaRecibir.enviar = (parseFloat(this.calcParaRecibir.recibir) + parseFloat(this.calcParaRecibir.comision)).toFixed(2);
 
   }
 
