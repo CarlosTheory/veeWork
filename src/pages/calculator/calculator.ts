940217cd71d4757269ef9ea7a84ae229b5a7e497
Carlos Aponte <apontec83@gmail.com>
2020-08-13 01:01:47 -0400

    Clear and Copy functions working good

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { registerLocaleData } from '@angular/common';
 import es from '@angular/common/locales/es';
+import { Clipboard } from '@ionic-native/clipboard';
+import { Toast } from '@ionic-native/toast';
+
 
 
 /**
@@ -29,7 +32,12 @@ export class CalculatorPage {
 		enviar: "",
 		comision:""
 	}
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private clipboard: Clipboard,
+    private toast: Toast
+  ) {
 
   }
 
@@ -42,25 +50,83 @@ export class CalculatorPage {
   }
 
   calcularEnvio(){
-  	if(this.calcParaEnviar.enviar <=0){
-  		return;
-  	}
-  	this.calcParaEnviar.enviar = Number(this.calcParaEnviar.enviar);
-	this.calcParaEnviar.comision = (((this.porcentaje * this.calcParaEnviar.enviar)/100) + this.adicional).toFixed(2);
-	this.calcParaEnviar.recibir = (parseFloat(this.calcParaEnviar.enviar) - parseFloat(this.calcParaEnviar.comision)).toFixed(2);
-	//console.log("Comision:"+this.calcParaEnviar.comision);
+  	if(this.calcParaEnviar.enviar <=0 || this.calcParaEnviar.enviar == ""){
+      this.calcParaEnviar= {
+        enviar: "",
+        recibir: "",
+        comision:""
+      }
+  	}else{
+      this.calcParaEnviar.enviar = Number(this.calcParaEnviar.enviar);
+      this.calcParaEnviar.comision = (((this.porcentaje * this.calcParaEnviar.enviar)/100) + this.adicional).toFixed(2);
+      this.calcParaEnviar.recibir = (parseFloat(this.calcParaEnviar.enviar) - parseFloat(this.calcParaEnviar.comision)).toFixed(2);
+      //console.log("Comision:"+this.calcParaEnviar.comision);
+    }
   }
 
   calcularRecibo(){
-	console.log("calcularRecibo()");
-	var r = (100 - this.porcentaje)/100;
-	this.calcParaRecibir.recibir = Number(this.calcParaRecibir.recibir);
-	this.calcParaRecibir.enviar = ((parseFloat(this.calcParaRecibir.recibir) + this .adicional)/r).toFixed(2);
-	this.calcParaRecibir.comision = (parseFloat(this.calcParaRecibir.enviar) - parseFloat(this.calcParaRecibir.recibir)).toFixed(2);
+    if(this.calcParaRecibir.recibir <= 0 || this.calcParaRecibir.recibir == ""){
+      this.calcParaRecibir= {
+        recibir: "",
+        enviar: "",
+        comision:""
+      }
+    } 
+    else {
+      var r = (100 - this.porcentaje)/100;
+      this.calcParaRecibir.recibir = Number(this.calcParaRecibir.recibir);
+      this.calcParaRecibir.enviar = ((parseFloat(this.calcParaRecibir.recibir) + this .adicional)/r).toFixed(2);
+      this.calcParaRecibir.comision = (parseFloat(this.calcParaRecibir.enviar) - parseFloat(this.calcParaRecibir.recibir)).toFixed(2);
+    }
   }
 
   hola(){
   	console.log("hola");
+  }
+
+  cleanInput(campo){
+    console.log(campo);
+    if(campo === "recibir"){
+      this.calcParaRecibir.recibir = "";
+      this.calcularRecibo();
+    } 
+    if(campo === "enviar"){
+      this.calcParaEnviar.enviar = "";
+      this.calcularEnvio();
+    } 
+    else {
+      return;
+    }
+  }
+
+  copyAmount(label, amount){
+    this.clipboard.copy(label+" $"+amount);
+
+    this.clipboard.paste().then(
+      (resolve: string) => {
+        this.toast.show('Copiado', '5000', 'center').subscribe(msg => {
+          console.log(msg);
+        });
+      },
+      (reject: string) => {
+        console.log('Error: '+reject);
+      }
+    );
+
+/*    console.log("Copy Amount")
+    if(bloque === "recibo"){
+      console.log("Bloque de recibo");
+      if(this.calcParaRecibir.hasOwnProperty(campo)){
+        console.log("calcParaRecibir", campo);
+      }
+    }if(bloque === "envio"){
+      console.log("Bloque de envio");
+      if(this.calcParaEnviar.hasOwnProperty(campo)){
+        console.log("calcParaEnviar", campo);
+      }else{
+          console.log("sino");
+      }
+    }*/
   }
 
 }
